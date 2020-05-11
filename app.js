1065f9c41bda721f74d576433a61c3c2db0d45ff
rucha-sd <52367895+rucha-sd@users.noreply.github.com>
2020-05-11 11:49:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -9,11 +9,22 @@ const port=process.env.PORT || 3000
 
 app.use(express.json())
 app.use(UserRouter)
+app.set("view engine", "ejs");
 app.get('',(req,res)=>{
     res.send("Hello Wordl...!!!")
 })
 
+
+
 app.get('\id')
+app.get('/all',(req,res,next) =>{
+    //Here fetch data using mongoose query like
+    User.find({}, function(err, users) {
+    if (err) throw err;
+    res.render('index',{users:users});
+  })
+})
+
 app.listen(port,()=>{
     console.log("This is Server "+port)
 })
