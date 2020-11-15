0d0c805117a61c8da0a2a04b47735e10ebf46d27
RohitPawar2406 <rohitpawar2406@gmail.com>
2020-11-15 21:42:31 +0530

    Fourth Commit

--- models/user.js
+++ models/user.js
@@ -36,10 +36,30 @@ userSchema.methods.givingTemperatureAndTime_Date=  async function(){
     console.log("Inside Methods...!!!")
     const user=this
     var temp="Default Temperature"
-    var today = new Date();
-    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var today = new Date()
+    if((today.getMonth()+1)<10 && (today.getDate())<10)
+    {
+        var date=today.getFullYear()+'-'+'0'+(today.getMonth()+1)+'-'+'0'+today.getDate()
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+        user.Temperature = user.Temperature.concat({temp,date,time})
+    }
+    else if((today.getMonth()+1)<10 && (today.getDate())>=10) 
+    {
+        var date=today.getFullYear()+'-'+'0'+(today.getMonth()+1)+'-'+today.getDate()
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+        user.Temperature = user.Temperature.concat({temp,date,time})
+    }
+    else if((today.getMonth()+1)>=10 && (today.getDate())<10)
+    {
+        var date=today.getFullYear()+'-'+(today.getMonth()+1)+'-'+'0'+today.getDate()
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+        user.Temperature = user.Temperature.concat({temp,date,time})
+    }
+    else{
+    var date=today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
     user.Temperature = user.Temperature.concat({temp,date,time})
+    }    
     await user.save()
     return temp
 }
--- router/router.js
+++ router/router.js
@@ -63,7 +63,7 @@ router.get('/Name',async(req,res)=>{
 router.get('/test',async(req,res)=>{
 
     //Dates Range in this Code 
-    var object={first:req.body.startDate,second :endDate}
+    var object={first:req.body.startDate,second :req.body.endDate}
 
 var datetime = require('node-datetime')
 var dt = datetime.create(object.first)
@@ -84,34 +84,31 @@ console.log("Total Dates in range is form of Array "+rangeOfdates)
 //Above Code Gives you a Array which will contain all dates in that range of start Date and 
 //End date in a form of array. That all dates are in form of string in rangeOfDates Array.
 
-
-    const DD = "2-5-2020"
-    var simpleArray=[]
-    console.log("Type of DD is "+typeof(DD))
+    var obj={}
+    var SimpleArray=[]
     try{
-        const user= await User.find({})
-        const user1=user.map((variable)=>{
-            console.log("This is information is about "+variable.name)
-            const user2=variable.Temperature.map((variable2)=>{
-                if(variable2.date === DD)
-                { var fullInfo=variable2.temp}
-                return fullInfo
-               
-            }) 
-            //console.log("Value of user2 is "+user2)  
-            user2.forEach((varr)=>{
-                //console.log(varr)
-                if(varr !== undefined) {
-                    //console.log(variable.name, varr)
-                    var Info=simplefunction(variable.name,varr)
-                    console.log("Type of Info is "+typeof Info) 
-                   //console.log("Value of simplearray is inside "+simpleArray)
-                }     
-            })
-        })
-        //console.log("Final Value of user is=="+user1)
+        var user=await User.find({})
+        rangeOfdates.forEach(element => {
+            var user11=user.map((variable)=>{
+                console.log("This is info of "+variable.name)
+                var user2=variable.Temperature.map((variable2)=>{
+                    console.log("Value of Element is "+element+"And variable2.date is "+variable2.date)
+                    console.log(element==variable2.date)
+                    if(element==variable2.date)
+                    {
+                        console.log(variable2.temp,variable2.time,variable2.date)
+                        obj={Tempo:variable2.temp,Time:variable2.time, CuurentDate:variable2.date}
+                        SimpleArray.push(obj)
+                        console.log(SimpleArray)
+                    }
+                })
         
-        }catch(e){
+                })
+        })
+
+        console.log("value of SimpleArray is "+SimpleArray)
+        res.send(SimpleArray)
+    }catch(e){
             res.status(400).send('Unable to get Data...!!')
         }
 
